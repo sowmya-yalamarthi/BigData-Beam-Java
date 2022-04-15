04bf1b49dd00c6f9af6ec5ada0db79d181773324
sowmya-yalamarthi <77811333+sowmya-yalamarthi@users.noreply.github.com>
2022-04-15 13:53:42 -0500

    Update MinimalPageRankSowmya.java

--- Sowmya/MinimalPageRankSowmya.java
+++ Sowmya/MinimalPageRankSowmya.java
@@ -15,7 +15,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package nwmissouri.bigdata.java.grp03_sowmya;
+package edu.nwmissouri.bigdata.java.grp03_sowmya;
 
 // beam-playground:
 //   name: MinimalWordCount
