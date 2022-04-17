cc5da3befb002f4744b3a2fe4ba9ebb62e235038
Saisumithra <77755086+Saisumithra@users.noreply.github.com>
2022-04-17 18:47:07 -0500

    kv pair

--- Saisumithra/src/main/java/edu/nwmissouri/group03/MinimalPageRankSumithra.java
+++ Saisumithra/src/main/java/edu/nwmissouri/group03/MinimalPageRankSumithra.java
@@ -15,7 +15,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package edu.nwmissouri.group03.sumithra;
+package edu.nwmissouri.group03;
 
 // beam-playground:
 //   name: MinimalWordCount
