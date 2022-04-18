9034c8183cddb7e84a7af7562ec0adc278e64e99
sowmya-yalamarthi <s542049@nwmissouri.edu>
2022-04-18 11:34:38 -0500

    changes made

--- Sowmya/src/main/java/edu/nwmissouri/bigdata/java/grp03_sowmya/RankedPage.java
+++ Sowmya/src/main/java/edu/nwmissouri/bigdata/java/grp03_sowmya/RankedPage.java
@@ -3,7 +3,5 @@ package edu.nwmissouri.bigdata.java.grp03_sowmya;
 import java.util.ArrayList;
 
 public class RankedPage {
-    public RankedPage(String key, ArrayList<VotingPage> voters) {
-    }
-
+    
 }
--- Sowmya/src/main/java/edu/nwmissouri/bigdata/java/grp03_sowmya/VotingPage.java
+++ Sowmya/src/main/java/edu/nwmissouri/bigdata/java/grp03_sowmya/VotingPage.java
@@ -1,7 +1,5 @@
 package edu.nwmissouri.bigdata.java.grp03_sowmya;
 
 public class VotingPage {
-    public VotingPage(String voterName, Integer contributorVotes) {
-    }
-
+    
 }
