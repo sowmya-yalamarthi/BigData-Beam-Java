463826448703a21032673db905a3b9e3761c3641
sowmya-yalamarthi <s542049@nwmissouri.edu>
2022-04-19 17:58:57 -0500

    changes made to pagerank

--- Sowmya/src/main/java/edu/nwmissouri/bigdata/java/grp03_sowmya/RankedPage.java
+++ Sowmya/src/main/java/edu/nwmissouri/bigdata/java/grp03_sowmya/RankedPage.java
@@ -2,6 +2,8 @@ package edu.nwmissouri.bigdata.java.grp03_sowmya;
 
 import java.util.ArrayList;
 
+
+
 public class RankedPage {
     
 }
--- Sowmya/src/main/java/edu/nwmissouri/bigdata/java/grp03_sowmya/VotingPage.java
+++ Sowmya/src/main/java/edu/nwmissouri/bigdata/java/grp03_sowmya/VotingPage.java
@@ -1,5 +1,7 @@
 package edu.nwmissouri.bigdata.java.grp03_sowmya;
 
 public class VotingPage {
+    public VotingPage(string voterName, string contributorVotes)
+    {}
     
 }
