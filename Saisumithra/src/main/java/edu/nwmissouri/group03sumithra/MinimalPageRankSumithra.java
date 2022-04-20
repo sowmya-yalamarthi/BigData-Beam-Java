f7a5407a2a856e3ef3cb29ff187b36a48b490cb3
Saisumithra <77755086+Saisumithra@users.noreply.github.com>
2022-04-19 21:23:36 -0500

    added job1finalizer

--- Saisumithra/src/main/java/edu/nwmissouri/group03sumithra/MinimalPageRankSumithra.java
+++ Saisumithra/src/main/java/edu/nwmissouri/group03sumithra/MinimalPageRankSumithra.java
@@ -29,22 +29,48 @@ package edu.nwmissouri.group03sumithra;
 //     - Core Transforms
 
 import java.util.Arrays;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Collection;
+
 import org.apache.beam.sdk.Pipeline;
 import org.apache.beam.sdk.io.TextIO;
 import org.apache.beam.sdk.options.PipelineOptions;
 import org.apache.beam.sdk.options.PipelineOptionsFactory;
 import org.apache.beam.sdk.transforms.Count;
+import org.apache.beam.sdk.transforms.DoFn;
 import org.apache.beam.sdk.transforms.Filter;
 import org.apache.beam.sdk.transforms.FlatMapElements;
 import org.apache.beam.sdk.transforms.Flatten;
+import org.apache.beam.sdk.transforms.GroupByKey;
 import org.apache.beam.sdk.transforms.MapElements;
+import org.apache.beam.sdk.transforms.ParDo;
 import org.apache.beam.sdk.values.KV;
 import org.apache.beam.sdk.values.PCollection;
 import org.apache.beam.sdk.values.PCollectionList;
+import org.apache.beam.sdk.values.PCollectionTuple;
 import org.apache.beam.sdk.values.TypeDescriptor;
 import org.apache.beam.sdk.values.TypeDescriptors;
 
 public class MinimalPageRankSumithra {
+
+  static class Job1Finalizer extends DoFn<KV<String, Iterable<String>>, KV<String, RankedPage>> {
+    @ProcessElement
+    public void processElement(@Element KV<String, Iterable<String>> element,
+        OutputReceiver<KV<String, RankedPage>> receiver) {
+      Integer contributorVotes = 0;
+      if (element.getValue() instanceof Collection) {
+        contributorVotes = ((Collection<String>) element.getValue()).size();
+      }
+      ArrayList<VotingPage> voters = new ArrayList<VotingPage>();
+      for (String voterName : element.getValue()) {
+        if (!voterName.isEmpty()) {
+          voters.add(new VotingPage(voterName, contributorVotes));
+        }
+      }
+      receiver.output(KV.of(element.getKey(), new RankedPage(element.getKey(), voters)));
+    }
+  }
 
   public static void main(String[] args) {
 
--- Saisumithra/src/main/java/edu/nwmissouri/group03sumithra/RankedPage.java
+++ Saisumithra/src/main/java/edu/nwmissouri/group03sumithra/RankedPage.java
@@ -1,9 +1,35 @@
 package edu.nwmissouri.group03sumithra;
 
+import java.io.Serializable;
+import java.util.ArrayList;
 
-
-public class RankedPage {
-
+public class RankedPage implements Serializable{
+    String voter;
+    ArrayList<VotingPage> voterList = new ArrayList<>();
     
+    public RankedPage(String voter, ArrayList<VotingPage> voters){
+        this.voter = voter;
+        this.voterList = voters;
+    }
+    
+    public String getVoter() {
+        return voter;
+    }
 
+    public void setVoter(String voter) {
+        this.voter = voter;
+    }
+
+    public ArrayList<VotingPage> getVoterList() {
+        return voterList;
+    }
+
+    public void setVoterList(ArrayList<VotingPage> voterList) {
+        this.voterList = voterList;
+    }
+
+    @Override
+    public String toString(){
+        return voter + voterList;
+    }
 }
